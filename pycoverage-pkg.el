0a3dc0cfbaafb0b5354fbf66d206c6f1db09f046
Matt Harrison <matthewharrison@gmail.com>
2016-03-24 09:29:53 -0600

    Update package info

--- pycoverage-pkg.el
+++ pycoverage-pkg.el
@@ -1,1 +1,1 @@
-(define-package "pycoverage" "20160104" "Support for coverage stats on Python 2.X" 'nil)
+(define-package "pycoverage" "20160323" "Support for coverage stats on Python 2.X and 3" 'nil)
